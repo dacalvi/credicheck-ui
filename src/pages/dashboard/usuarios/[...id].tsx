1c2caacea8f0e6ae3c6a1d0808e605ba93d97eee
Daniel Calvi <123417176+dcalvivault@users.noreply.github.com>
2023-06-23 11:20:56 -0300

    Actualizar  en vez de Crear en formulario de edit

--- src/pages/dashboard/usuarios/[...id].tsx
+++ src/pages/dashboard/usuarios/[...id].tsx
@@ -260,7 +260,7 @@ const Index: React.FC = () => {
                 <button
                   type="submit"
                   className="inline-flex justify-center px-3 py-2 ml-3 text-sm font-medium text-white bg-blue-500 border border-transparent shadow-sm rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                  Crear Usuario
+                  Actualizar Usuario
                 </button>
               </div>
             </form>
