7784942269b0c4c9d2828275c26d9b67cfb13ead
Daniel Calvi <123417176+dcalvivault@users.noreply.github.com>
2023-08-23 00:34:50 -0300

    test

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,4 @@
-import {Spinner} from "flowbite-react";
-
 const Index: React.FC = () => {
-  return (
-    <>
-      <div className="flex">
-        <Spinner color="info" aria-label="Info spinner example" />
-        <div className="ml-2 mt-1">Redirigiendo al tablero...</div>
-      </div>
-    </>
-  );
+  return <>test</>;
 };
 export default Index;
