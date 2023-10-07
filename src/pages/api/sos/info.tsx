f143a5c3fae84388ce60bcb548fb7aa621ff90be
Daniel Calvi <123417176+dcalvivault@users.noreply.github.com>
2023-10-07 12:30:29 -0300

    info

--- src/pages/api/sos/info.tsx
+++ src/pages/api/sos/info.tsx
@@ -1,6 +1,12 @@
+import {get_url} from "functions/helpers";
+
 export default async function handler(req: any, res: any) {
   if (req.method === "GET") {
-    return res.status(200).json({message: "Hello, world", success: true});
+    return res.status(200).json({
+      message: "Hello, world",
+      url: get_url(),
+      success: true,
+    });
   } else {
     return res
       .status(405)
